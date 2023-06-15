3a2c72a83aee059ab87cb825878c653b150eccbe
Jayden Remillard <ja.d.remillard@baypath.net>
2023-06-15 13:23:41 -0400

    Fixed it

--- LargestPrimeFactor.java
+++ LargestPrimeFactor.java
@@ -4,13 +4,35 @@ public class LargestPrimeFactor {
         Scanner line = new Scanner(System.in);
         System.out.println("Input a number less than " + Integer.MAX_VALUE + " and greater than " + Integer.MIN_VALUE + ":");
         int num = line.nextInt();
-        line.close();
-        for(int i = 3; i < num; i+=2) {
-            if(num % i == 0) {
-                num /= i;
-                i = 3;
-            }
+        while(num == 1) {
+            System.out.println("You're stupid. 1 isn't prime or non-prime. Try again.");
+            System.out.println("Input a number less than " + Integer.MAX_VALUE + " and greater than " + Integer.MIN_VALUE + ":");
+            num = line.nextInt();
         }
+        line.close();
+        if(num > 0) {
+            for(int i = 3; i < num; i+=2) {
+                if(num%2 == 0) {
+                    num /= 2;
+                    i = 3;
+                }
+                if(num % i == 0) {
+                    num /= i;
+                    i = 3;
+                }
+            }
+        } else if(num < 0) {
+            for(int i = -3; i > num; i-=2) {
+                if(num%2 == 0) {
+                    num /= 2;
+                    i = -3;
+                }
+                if(num % i == 0) {
+                    num /= -i;
+                    i = -3;
+                }
+            }
+        } else { num = 0; }
         System.out.println(num);
     }
 }
